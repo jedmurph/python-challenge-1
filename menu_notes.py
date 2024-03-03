23b8d49650d9bda3a93ee65087821e46a7717e37
Jed <jed.murphy@du.edu>
2024-03-03 12:29:39 -0700

    first commit from menu

---
+++ menu_notes.py
@@ -0,0 +1,24 @@
+#fruits = ["apples",  "bananas", "cherry"]
+#for x in fruits:
+ #   print(x)
+
+#for x in "bananas":
+    #print(x)
+
+
+    #def process_payment():
+    #    print("The total cost of this transaction will be 75 cents.")
+    #    print("Ka-ching! Payment has been processed.")
+
+    #if __name__ == "__main__":
+     #   process_payment()
+
+def add():
+    """This function takes two numbers and adds them and then returns the total."""
+    first_number = 1 
+    second_number = 2
+    print(f"Your total is: {total}")
+
+if __name__ == "__main__":
+    add()
+    print(first_number)
